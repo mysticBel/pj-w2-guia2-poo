71f84b38587c3a6ec14735b0ebce41d691c2fe0e
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2022-09-25 17:52:02 -0500

    Asignar el precio de alquiler

---
+++ pj-w2-guia2/Servicealquiler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pj_w2_guia2
+{
+    internal class Servicealquiler
+    {
+        public int numAlquiler { get; set; }
+        public string equipo { get; set; }
+        public int numDias { get; set; }
+
+
+        // 1. Asignar el precio de alquiler
+        public double asignarPrecio() {
+
+            switch (equipo) 
+            { 
+                case "Teclado": return 20;
+                case "Monitor": return 100;
+                case "Mouse": return 20;
+                case "Proyector": return 200;
+            }
+            return 0;
+        }
+    }
+}
