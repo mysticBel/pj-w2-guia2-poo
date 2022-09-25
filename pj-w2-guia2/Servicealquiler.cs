f7dd534755ca869e1ace946ce7da7c98520052c6
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2022-09-25 18:05:48 -0500

    Implementando metodos principales

--- pj-w2-guia2/Servicealquiler.cs
+++ pj-w2-guia2/Servicealquiler.cs
@@ -16,8 +16,8 @@ namespace pj_w2_guia2
         // 1. Asignar el precio de alquiler
         public double asignarPrecio() {
 
-            switch (equipo) 
-            { 
+            switch (equipo)
+            {
                 case "Teclado": return 20;
                 case "Monitor": return 100;
                 case "Mouse": return 20;
@@ -25,5 +25,28 @@ namespace pj_w2_guia2
             }
             return 0;
         }
+
+        //Constructor - inicializa el nro de dias, si el usuario no registra el nro 
+        // de dias alquilados, se asignará como 1 
+        public Servicealquiler(){
+            this.numDias = 1;
+        }
+
+        //2.calcular el subtotal a pagar por los días alquilados,
+        public double calcularSubtotal() { 
+            return numDias * asignarPrecio();
+        }
+
+        //3. calcular el dscto del 5% todos los equipos solo para la sede central.
+        //-no se ingresa el precio, ya se tiene una tabla de precios
+        public double calcularDescuento(){
+            return 0.05 * calcularSubtotal();
+        }
+
+        //Calcular neto
+        public double calcularNeto() {
+            return calcularSubtotal() - calcularDescuento();
+        }
+
     }
 }
