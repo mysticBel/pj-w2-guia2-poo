5be5752c29434a5bf22c62ddf5eb0c9ab414bbe6
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2022-09-25 21:50:19 -0500

    frm Diseño listo :D

---
+++ pj-w2-guia2/frmPlazaNorte.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace pj_w2_guia2
+{
+    partial class frmPlazaNorte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboEquipo = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDias = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lvAlquiler = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PeachPuff;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(801, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(146, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(423, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONTROL DE ALQUILER DE EQUIPOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NUMERO";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(36, 130);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(40, 18);
+            this.lblNumero.TabIndex = 1;
+            this.lblNumero.Text = "0000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(148, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "EQUIPO";
+            // 
+            // cboEquipo
+            // 
+            this.cboEquipo.FormattingEnabled = true;
+            this.cboEquipo.Location = new System.Drawing.Point(151, 131);
+            this.cboEquipo.Name = "cboEquipo";
+            this.cboEquipo.Size = new System.Drawing.Size(169, 21);
+            this.cboEquipo.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(345, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "DIAS DE ALQUILER";
+            // 
+            // txtDias
+            // 
+            this.txtDias.Location = new System.Drawing.Point(348, 133);
+            this.txtDias.Name = "txtDias";
+            this.txtDias.Size = new System.Drawing.Size(132, 20);
+            this.txtDias.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(578, 104);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(180, 49);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // lvAlquiler
+            // 
+            this.lvAlquiler.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lvAlquiler.GridLines = true;
+            this.lvAlquiler.HideSelection = false;
+            this.lvAlquiler.Location = new System.Drawing.Point(29, 190);
+            this.lvAlquiler.Name = "lvAlquiler";
+            this.lvAlquiler.Size = new System.Drawing.Size(729, 234);
+            this.lvAlquiler.TabIndex = 5;
+            this.lvAlquiler.UseCompatibleStateImageBehavior = false;
+            this.lvAlquiler.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "N°";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "EQUIPO";
+            this.columnHeader2.Width = 160;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "DIAS ALQUILADOS";
+            this.columnHeader3.Width = 111;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "PRECIO ALQUILER";
+            this.columnHeader4.Width = 119;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "SUBTOTAL";
+            this.columnHeader5.Width = 97;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "DESCUENTO";
+            this.columnHeader6.Width = 91;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "NETO";
+            this.columnHeader7.Width = 84;
+            // 
+            // frmPlazaNorte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 436);
+            this.Controls.Add(this.lvAlquiler);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtDias);
+            this.Controls.Add(this.cboEquipo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmPlazaNorte";
+            this.Text = "frmPlazaNorte";
+            this.Load += new System.EventHandler(this.frmPlazaNorte_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboEquipo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDias;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListView lvAlquiler;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
---
+++ pj-w2-guia2/frmPlazaNorte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pj_w2_guia2
+{
+    public partial class frmPlazaNorte : Form
+    {
+        public frmPlazaNorte()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPlazaNorte_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
